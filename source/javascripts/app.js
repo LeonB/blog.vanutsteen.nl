f4738c470362dc9445221c6428b76b938c6e4df7
Leon Bogaert <leon@tim-online.nl>
2013-02-11 00:56:57 +0100

    Fixed fancybox

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,11 +1,13 @@
-//= require "jquery-1.9.1"
-//= require "jquery.fancybox-2.1.4"
+//= require "vendor/jquery-1.9.1"
+//= require "vendor/jquery.fancybox-2.1.4"
+
+jQuery.noConflict();
 
 jQuery(document).ready(function(){
 	jQuery('.entry-content').find('a[href$=".jpg"], a[href$=".png"]').find('img').each(function(i, img){
 		var img = jQuery(img);
 		var a = img.parent();
-		a.fancybox();
+		a.fancybox({});
 
 	});
 })
