06ccb15f5b6fae1f15abdfbb275d53f65c3d26bc
Leon Bogaert <leon@tim-online.nl>
2013-02-17 20:24:14 +0100

    Match middleman norms

--- lib/html_filters.rb
+++ lib/html_filters.rb
@@ -1,15 +1,19 @@
 # require "html_truncator"
 
 module HtmlFilters
+  class << self
+    def registered(app)
+      app.helpers Helpers
+    end
+    alias :included :registered
+  end
 
-  # Like the Rails _truncate_ helper but doesn't break HTML tags or entities.
-  # def truncate_html(text, max_length = 30, ellipsis = "...")
-  #   return if text.nil?
-  #   return HTML_Truncator.truncate(text, max_length, ellipsis)
-  # end
-
+  module Helpers
   def strip_img(raw)
     raw.gsub(/<img.*\/>/, '')
   end
+  end
 
 end
+
+::Middleman::Extensions.register(:html_filters, HtmlFilters)
