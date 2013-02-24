9178a0312c4a0b7bba4edec18c34375718eef484
Leon Bogaert <leon@tim-online.nl>
2013-02-24 14:45:39 +0100

    Started working on custom js search

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,5 +1,7 @@
 //= require "vendor/jquery-1.9.1"
 //= require "vendor/jquery.fancybox-2.1.4"
+//= require "vendor/jquery.ba-hashchange.min.js"
+//= require "vendor/jquery.swiftype.search.js"
 
 jQuery.noConflict();
 
@@ -8,6 +10,29 @@ jQuery(document).ready(function(){
 		var img = jQuery(img);
 		var a = img.parent();
 		a.fancybox({});
+	});
 
+	jQuery('#st-search-input').swiftypeSearch({
+    	engineKey: 'rkpFqoWSMw9vXMnz3NPw',
+        perPage: 20
 	});
 })
+
+
+
+// var Swiftype = window.Swiftype || {};
+// (function() {
+// 	Swiftype.key = 'rkpFqoWSMw9vXMnz3NPw';
+// 	Swiftype.inputElement = '#st-search-input';
+// 	Swiftype.resultContainingElement = '#st-results-container';
+// 	Swiftype.attachElement = '#st-search-input';
+// 	Swiftype.renderStyle = "inline";
+// 	Swiftype.resultPageURL = '/search/';
+
+// 	var script = document.createElement('script');
+// 	script.type = 'text/javascript';
+// 	script.async = true;
+// 	script.src = "//swiftype.com/embed.js";
+// 	var entry = document.getElementsByTagName('script')[0];
+// 	entry.parentNode.insertBefore(script, entry);
+// }());
